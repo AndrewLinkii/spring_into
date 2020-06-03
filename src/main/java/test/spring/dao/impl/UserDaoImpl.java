1c9459a1beb33b43fe6e5438a78e6dc1cdbc6fda
Andrew <tolstuy1996@gmail.com>
2020-06-03 16:28:26 +0300

    add travis

--- src/main/java/test/spring/dao/impl/UserDaoImpl.java
+++ src/main/java/test/spring/dao/impl/UserDaoImpl.java
@@ -3,6 +3,7 @@ package test.spring.dao.impl;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -17,10 +18,21 @@ public class UserDaoImpl implements UserDao {
 
     @Override
     public void add(User user) {
-        try (Session session = sessionFactory.openSession()) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
             session.save(user);
+            transaction.commit();
         } catch (Exception e) {
-            throw new RuntimeException("Can't save user", e);
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 
